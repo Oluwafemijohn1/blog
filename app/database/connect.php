a7daeb394314df50c02567aa9a2afc511db83bc9
Oluwafemijohn <femiogundipe01@gmail.com>
2020-09-22 09:33:40 +0100

    database move online

--- app/database/connect.php
+++ app/database/connect.php
@@ -1,8 +1,17 @@
 <?php
-$host = 'localhost';
-$user = 'root';
-$pass = 'emmanuel001.(ephemzy)';
-$db_name = 'blog';
+
+// In development
+// $host = 'localhost';
+// $user = 'root';
+// $pass = 'emmanuel001.(ephemzy)';
+// $db_name = 'blog';
+
+
+//heroku
+$host = 'remotemysql.com';
+$user = 'yHZSEwccB0';
+$pass = 'TSZx2fMZEF';
+$db_name = 'yHZSEwccB0';
 
 $conn = new MySQLi( $host, $user, $pass, $db_name);
 
