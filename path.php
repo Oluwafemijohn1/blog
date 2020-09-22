7c1e30a6f25dde470fbb4d6813a695d5fa8b3a49
Oluwafemijohn <femiogundipe01@gmail.com>
2020-09-22 19:49:21 +0100

    path edited

--- path.php
+++ path.php
@@ -1,5 +1,6 @@
 <?php 
 
 define("ROOT_PATH", realpath(dirname(__FILE__)));
-define("BASE_URL", "http://localhost/blog/blog/");
+//define("BASE_URL", "http://localhost/blog/blog/");
+define("BASE_URL", "https://mi-blog-app.herokuapp.com/");
 
